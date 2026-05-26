1f0b244e6671300d093eb4c8e8dd4a83be334c4b
JavCub <157952473+JavCub@users.noreply.github.com>
2026-05-26 11:17:50 +0800

    Update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -66,7 +66,7 @@ export function Hero() {
                 ease: 'easeInOut',
               }
             }}
-            className="h-40 w-40 md:h-48 md:w-48 rounded-full overflow-hidden shadow-2xl"
+            className="h-56 w-56 md:h-64 md:w-64 rounded-full overflow-hidden shadow-2xl"
           >
             <picture>
               <source srcSet={pfpAvif} type="image/avif" />
